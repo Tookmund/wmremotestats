fb9048b3d989d5f1c8ae961aa1a8e162d66b63cd
Jacob Adams <jacob@tookmund.com>
2020-07-25 00:19:00 -0400

    Add per-department statistics

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-const b2 = "https://f000.backblazeb2.com/file/wmcoursescraper"
-d3.csv(`${b2}/subjects.csv`).then(data => {
-		data.splice(0, 0, { "Full": "All", "Short": ""});
-		d3.select("#dept")
-			.selectAll("option")
-			.data(data)
-			.enter()
-			.append("option")
-				.text(d => d.Full)
-				.attr("value", (d, i) => d.Short);
-});
+function newDelivery () {
+	return JSON.parse(JSON.stringify({"FS": 0, "MIX": 0, "RA": 0, "RSOC": 0, "RSOF": 0});
+}
+var delivery = null;
+var deptdeliver = null;
 
-d3.csv(`${b2}/Fall2020.csv`).then(data => {
-	var delivery = {"FS": 0, "MIX": 0, "RA": 0, "RSOC": 0, "RSOF": 0};
-	data.forEach(d => {
-		for (const p in delivery) {
-			if (d.Attributes.includes(p)) {
-				delivery[p] += 1;
-			}
-		}
-	});
-	d3.select("body")
-		.append("div")
-		.selectAll("p")
-		.data(Object.keys(delivery))
-		.enter()
-		.append("p")
-			.text(d => `${d}: ${delivery[d]}`);
+function updateGraph(dept) {
+	var data;
+	if (dept == "") data = delivery;
+	else data = deptdeliver[dept];
 
 	var margin = {top: 20, right: 20, bottom: 30, left: 40},
 	width = 960 - margin.left - margin.right,
@@ -43,17 +25,17 @@ d3.csv(`${b2}/Fall2020.csv`).then(data => {
 			.attr("transform",
 				"translate(" + margin.left + "," + margin.top + ")");
 	// Scale the range of the data in the domains
-	x.domain(Object.keys(delivery));
-	y.domain([0, d3.max(Object.values(delivery))]);
+	x.domain(Object.keys(data));
+	y.domain([0, d3.max(Object.values(data))]);
 	// append the rectangles for the bar chart
 	svg.selectAll(".bar")
-		.data(Object.keys(delivery))
+		.data(Object.keys(data))
 		.enter().append("rect")
 			.attr("class", "bar")
 			.attr("x", d => x(d))
 			.attr("width", x.bandwidth())
-			.attr("y", d => y(delivery[d]))
-			.attr("height", d => height - y(delivery[d]));
+			.attr("y", d => y(data[d]))
+			.attr("height", d => height - y(data[d]));
 
 	// add the x Axis
 	svg.append("g")
@@ -63,4 +45,46 @@ d3.csv(`${b2}/Fall2020.csv`).then(data => {
 	// add the y Axis
 	svg.append("g")
 		.call(d3.axisLeft(y));
+}
+
+const b2 = "https://f000.backblazeb2.com/file/wmcoursescraper"
+d3.csv(`${b2}/subjects.csv`).then(data => {
+		data.splice(0, 0, { "Full": "All", "Short": ""});
+		d3.select("#dept")
+			.selectAll("option")
+			.data(data)
+			.enter()
+			.append("option")
+				.text(d => d.Full)
+				.attr("value", (d, i) => d.Short);
+
+		d3.select("#dept").on("change", () => {
+			var dept = d3.select("#dept").node().value;
+			updateGraph(dept);
+		});
+});
+
+d3.csv(`${b2}/Fall2020.csv`).then(data => {
+	delivery = newDelivery();
+	deptdeliver = {}
+	data.forEach(d => {
+		for (const p in delivery) {
+			if (d.Attributes.includes(p)) {
+				delivery[p] += 1;
+				if (!d.Subject in deptdeliver) {
+					deptdeliver[d.Subject] = newDelivery();
+				}
+				deptdeliver[d.Subject][p] += 1
+			}
+		}
+	});
+	d3.select("body")
+		.append("div")
+		.selectAll("p")
+		.data(Object.keys(delivery))
+		.enter()
+		.append("p")
+			.text(d => `${d}: ${delivery[d]}`);
+
+	updateGraph("");
 });
