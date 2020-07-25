7974842c616061d975419511452bc7d7f776c379
Jacob Adams <jacob@tookmund.com>
2020-07-24 23:48:26 -0400

    script: Correct all the tabbing

--- script.js
+++ script.js
@@ -10,45 +10,45 @@ d3.csv(`${b2}/subjects.csv`).then(data => {
 });
 
 d3.csv(`${b2}/Fall2020.csv`).then(data => {
-		var delivery = {"FS": 0, "MIX": 0, "RA": 0, "RSOC": 0, "RSOF": 0};
-		data.forEach(d => {
-			for (const p in delivery) {
-				if (d.Attributes.includes(p)) {
-					delivery[p] += 1;
-				}
+	var delivery = {"FS": 0, "MIX": 0, "RA": 0, "RSOC": 0, "RSOF": 0};
+	data.forEach(d => {
+		for (const p in delivery) {
+			if (d.Attributes.includes(p)) {
+				delivery[p] += 1;
 			}
-		});
-		d3.select("body")
-			.append("div")
-			.text(JSON.stringify(delivery));
+		}
+	});
+	d3.select("body")
+		.append("div")
+		.text(JSON.stringify(delivery));
 
-		var margin = {top: 20, right: 20, bottom: 30, left: 40},
-		width = 960 - margin.left - margin.right,
-		height = 500 - margin.top - margin.bottom;
+	var margin = {top: 20, right: 20, bottom: 30, left: 40},
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
 
-		var x = d3.scaleBand()
-			.range([0, width])
-			.padding(0.1);
-		var y = d3.scaleLinear()
-			.range([height, 0]);
-		var svg = d3.select("body").append("svg")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom)
-			.append("g")
-				.attr("transform",
-					"translate(" + margin.left + "," + margin.top + ")");
-		// Scale the range of the data in the domains
-		x.domain(Object.keys(delivery));
-		y.domain([0, Math.max(delivery.values())]);
+	var x = d3.scaleBand()
+		.range([0, width])
+		.padding(0.1);
+	var y = d3.scaleLinear()
+		.range([height, 0]);
+	var svg = d3.select("body").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+			.attr("transform",
+				"translate(" + margin.left + "," + margin.top + ")");
+	// Scale the range of the data in the domains
+	x.domain(Object.keys(delivery));
+	y.domain([0, Math.max(delivery.values())]);
 	// append the rectangles for the bar chart
 	svg.selectAll(".bar")
-			.data(Object.keys(delivery))
-			.enter().append("rect")
-				.attr("class", "bar")
-				.attr("x", d => x(d))
-				.attr("width", x.bandwidth())
-				.attr("y", d => y(delivery[d]))
-				.attr("height", d => height - y(delivery[d]));
+		.data(Object.keys(delivery))
+		.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", d => x(d))
+			.attr("width", x.bandwidth())
+			.attr("y", d => y(delivery[d]))
+			.attr("height", d => height - y(delivery[d]));
 
 	// add the x Axis
 	svg.append("g")
